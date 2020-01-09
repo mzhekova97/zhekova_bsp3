7a93a4db4a898c035ca09ec181b6ebf49a397c68
mzhekova97 <mzhekova97@gmail.com>
2020-01-09 13:15:19 +0100

    subscriber

---
+++ stack/sensors_reader/src/sensor_subscriber.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float32MultiArray
+import RPi.GPIO as GPIO
+
+def callback(msg):
+    rospy.loginfo(rospy.get_caller_id() + msg.data)
+
+def listener():
+    rospy.init_node'sensor_subscriber', anonymous=True)
+    rospy.Subscriber('distance_from_sensor',Float32MultiArray, callback)
+
+    rospy.spin()
+
+if __name__=='__main__':
+    try:
+        listener()
+    except rospy.ROSInterruptException:
+        rospy.loginfo('Measurement stopped by user.')
